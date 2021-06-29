14871d02b4e1dd1b45bce9e6d68fa6a2f9c09a14
Mari2807 <86535827+Mari2807@users.noreply.github.com>
2021-06-29 13:24:38 -0300

    Update exemplo14.js

--- js-basico/repostas/exemplo14.js
+++ js-basico/repostas/exemplo14.js
@@ -1,2 +1,2 @@
-//Imprima na tela os números pares existentes entre 0 e 100.
-console.log('Estou rodando no terminal')
+//Imprima na tela os números pares existentes entre 0 e 100
+console.log('estou rodando no terminal')
